aa8dc6b86ed5d636c66661eb48d3817e664334fa
Kyle McColgan <kdmbus21@gmail.com>
2024-05-06 19:15:40 -0500

    Add Event.java model class and package and update EventController.java and the index.html template.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -1,27 +1,36 @@
 package org.launchcode.codingevents.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.launchcode.codingevents.models.Event;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController
 {
-    private static HashMap<String, String> events = new HashMap<>(); //non-persistent, exists in memory of program only.
+    //private static HashMap<String, String> events = new HashMap<>(); //non-persistent, exists in memory of program only.
+    private static List<Event> events = new ArrayList<>();
     @GetMapping
     public String createList(Model model)
     {
-        events.put("Runtime exception", "An error occurred during execution.");
-        events.put("Load", "Program begins running.");
-        events.put("Quit", "Program quit running.");
-        events.put("Buffer overflow", "A program's memory capacity was exceeded.");
-        events.put("SQL Injection", "User input used to query the SQL DB.");
+//        events.put("Runtime exception", "An error occurred during execution.");
+//        events.put("Load", "Program begins running.");
+//        events.put("Quit", "Program quit running.");
+//        events.put("Buffer overflow", "A program's memory capacity was exceeded.");
+//        events.put("SQL Injection", "User input used to query the SQL DB.");
+        events.add(new Event("Runtime exception"));
+        events.add(new Event("Load"));
+        events.add(new Event("Quit"));
+        events.add(new Event("Buffer overflow"));
+        events.add(new Event("SQL Injection"));
         model.addAttribute("events",events);
         return "events/index";
     }
@@ -36,7 +45,8 @@ public class EventController
     public String createEvent(@RequestParam String eventName, @RequestParam String eventDesc) //parameter name must match the html element name for Spring Boot to work here.
     {
         //events.add(eventName);
-        events.put(eventName, eventDesc);
+        //events.put(eventName, eventDesc);
+        events.add(new Event(eventName));
         return "redirect:/events"; //Returns redirect response 300 level HTTP response...
         // the /events IS needed for the redirect view to properly work or else 404 errors.
     }
