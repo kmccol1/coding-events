ddd2ae541690fa288f0a3fd863585045955574d5
Kyle McColgan <kdmbus21@gmail.com>
2024-05-06 19:37:43 -0500

    Add an unique ID field to the new Event.java model class as well as a table column to the index.html.

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,7 +1,11 @@
 package org.launchcode.codingevents.models;
 
+import java.util.Objects;
+
 public class Event
 {
+    private int id;
+    private static int nextId = 1; //every obj shares same val...
     private String name;
     private String desc;
 
@@ -9,6 +13,8 @@ public class Event
     {
         this.name = name;
         this.desc = aDesc;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -27,9 +33,26 @@ public class Event
         this.desc = desc;
     }
 
+    public int getId() {
+        return id;
+    }
+
     @Override
     public String toString()
     {
         return this.getName();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
